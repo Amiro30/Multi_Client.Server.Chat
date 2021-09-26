269210cf2ae9d22462c0309341cdc39bb3f99b6e
Andrew Miroshnichenko <amiro30@i.ua>
2021-09-26 14:57:24 +0300

    db context ans models added

---
+++ Server/DbContext/ChatContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Server.Models;
+
+namespace Server.DbContext
+{
+    public class ChatContext : System.Data.Entity.DbContext
+    {
+        //Name of Database
+        public ChatContext() : base("ChatContext")
+        {}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ Server/Models/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Msg { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+
+        [ForeignKey("User")]
+        public int SenderId { get; set; }
+
+        [ForeignKey("User")]
+        public int RcptId { get; set; }
+
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
